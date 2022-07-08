33ac0c395ddb612cf23f4dc50bb07e9e7c3cddb0
erickmind <erick.kussumoto@gmail.com>
2022-07-08 18:47:28 -0300

    Chapter 4 started

--- Cap3/DataConversion.cs
+++ Cap3/DataConversion.cs
@@ -1,5 +1,6 @@
 using System;
 using static System.Console;
+using System.Globalization;
 
 namespace csharpbook{
     
@@ -22,10 +23,24 @@ namespace csharpbook{
             int example = (int)objExample;
             WriteLine($"Object type: {objExample.GetType()}");
             WriteLine($"Converted Int type: {example.GetType()}");
-            
-            WriteLine("---------- Parse -----------");
-            
 
+            WriteLine("---------- Parse -----------");
+            WriteLine($"{Int16.Parse("100")}");
+            WriteLine($"{Int16.Parse("(100)", NumberStyles.AllowParentheses)}");
+            WriteLine($"{Int32.Parse("50.000", NumberStyles.AllowThousands)}");
+            WriteLine($"Current System Culture: {CultureInfo.CurrentCulture.NativeName}");
+            WriteLine($"Current System Currency: {NumberFormatInfo.CurrentInfo.CurrencySymbol}");
+            WriteLine($"{int.Parse("R$532000", NumberStyles.AllowCurrencySymbol)}");
+            WriteLine($"{int.Parse("-200", NumberStyles.AllowLeadingSign)}");
+            WriteLine($"{int.Parse(" 500 ", NumberStyles.AllowLeadingWhite | NumberStyles.AllowTrailingWhite)}");
+
+            WriteLine("---------- Convert.To -----------");
+            decimal stringToDecimal = Convert.ToDecimal("12450,08");
+            WriteLine($"{stringToDecimal.GetType()} - {stringToDecimal}");
+            string decimalToString = Convert.ToString(2607.20m);
+            WriteLine($"{decimalToString.GetType()} - {decimalToString ?? "Text is null"}");
+            object? errorTest = null;
+            WriteLine($"{errorTest ?? "Text is null"}");
         }
     }
 }
---
+++ Cap4/DataCollections.cs
@@ -0,0 +1,25 @@
+using static System.Console;
+using System;
+using System.Collections.Generic;
+
+namespace csharpbook{
+    class dataCollections{
+        static void Main(string[] args){
+            //Arrays
+            int[] array = {1,2,3,4};
+            int[,] matrix = {{1,2,3,4}, {5,6,7,8}};
+            //Lists
+            List<string> names = new List<string>();
+            names.Add("Maria");
+            names.Add("Joao");
+            names.Add("Jose");
+            WriteLine(names[0]);
+            names.RemoveAt(0);
+            WriteLine(names[0]);
+
+            //Generic Types
+        }
+
+    }
+
+}
